099a026691e27211b9e8d2af33b0d55ad13dcc19
AnnaRamosV <anna212192@gmail.com>
2025-04-13 19:07:00 +0200

    Input personalizado

--- src/modulorum/projects/views/ProjectsView.vue
+++ src/modulorum/projects/views/ProjectsView.vue
@@ -10,7 +10,7 @@
           <th>Avance</th>
         </tr>
       </thead>
-      <tbody>
+      <body>
         <!-- row 2 -->
         <tr class="hover:bg-base-300">
           <th>2</th>
@@ -18,24 +18,31 @@
           <td>Desktop Support Technician</td>
           <td>Purple</td>
         </tr>
-      </tbody>
+      </body>
     </table>
   </div>
 
-  <InputModal />
+  <InputModal
+    :aperta="modalAperta"
+    @claudere="modalAperta = false"
+    @valorem="cumNovusValorem"
+    placeholder="Introduce el nombre del proyecto"
+    titulus="Nuevo proyecto"
+    subtitulus="Dale un nombre a tu proyecto"
+  />
 
-  <FabButton
-    @click="
-      () => {
-        console.log('clicked');
-      }
-    "
-  >
+  <fab-button positione="bottom-right" @click="modalAperta = true">
     <AddCircle />
-  </FabButton>
+  </fab-button>
 </template>
 <script lang="ts" setup>
 import FabButton from '@/modulorum/commune/components/FabButton.vue';
 import InputModal from '@/modulorum/commune/components/InputModal.vue';
 import AddCircle from '@/modulorum/commune/icons/AddCircle.vue';
+import { ref } from 'vue';
+
+const modalAperta = ref(false);
+const cumNovusValorem = (projectNomen: string) => {
+  console.log(projectNomen);
+};
 </script>
