7451fcf8ce7fb91d8adf28869346fb6777ede851
AnnaRamosV <anna212192@gmail.com>
2025-04-15 11:18:56 +0200

    Implementar BreadCrumb

---
+++ src/modulorum/projects/views/ProjectView.vue
@@ -0,0 +1,30 @@
+<template>
+  <BreadCrumbs :nomen="project?.nomen ?? 'No Name'" />
+</template>
+
+<script setup lang="ts">
+import BreadCrumbs from '@/modulorum/commune/components/BreadCrumbs.vue';
+import { useProjectsStore } from '../store/project.store';
+import { ref, watch } from 'vue';
+import type { Project } from '../interfaces/project.interface';
+
+interface Props {
+  id: string;
+}
+
+const props = defineProps<Props>();
+
+const projectStore = useProjectsStore();
+
+const project = ref<Project | null>();
+
+watch(
+  () => props.id,
+  () => {
+    project.value = projectStore.projectList.find((project) => project.id === props.id);
+  },
+  {
+    immediate: true,
+  },
+);
+</script>
